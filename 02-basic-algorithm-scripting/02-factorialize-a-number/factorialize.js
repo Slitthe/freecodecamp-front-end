ab89191c3439a8e937957e2a601393fac1eaee37
Silviu Gherman <sil.gherman@gmail.com>
2017-09-30 13:21:02 +0300

    Update and rename 02-basic-algorithm-scripting/02-factoralize-a-number/factoralize.js to 02-basic-algorithm-scripting/02-factorialize-a-number/factorialize.js

--- 02-basic-algorithm-scripting/02-factorialize-a-number/factorialize.js
+++ 02-basic-algorithm-scripting/02-factorialize-a-number/factorialize.js
@@ -1,15 +1,15 @@
-var factoralize = function (num) {
+var factorialize = function (num) {
 	// set the initial value to be 1,  so 0! -> 1 and 1! -> 2
-	var factoralize = 1;
+	var factorialize = 1;
 	// returns undefined for any negative number
 	if ( num < 0) {
-		return undefined; // Only positive integers can be factoralized
+		return undefined; // Only positive integers can be factorialized
 	}
 	else {
 		// for num parameter being 2 or higher, make the result a product of every integer between them and 2
 		for ( var i = num; i >= 2 ; i-- ) {
-			factoralize *= i;
+			factorialize *= i;
 		}
 	}
-	return factoralize;
+	return factorialize;
 };
