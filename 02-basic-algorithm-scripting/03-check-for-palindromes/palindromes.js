c349e86eaa42dbd03b18f6ad4755fc3c055f9aab
Silviu Gherman <sil.gherman@gmail.com>
2017-09-30 13:42:33 +0300

    Update palindromes.js

--- 02-basic-algorithm-scripting/03-check-for-palindromes/palindromes.js
+++ 02-basic-algorithm-scripting/03-check-for-palindromes/palindromes.js
@@ -1,16 +1,15 @@
 function palindrome (str) {
-	var expression = /[^a-z0-9]/g;// expression which finds anything that's not a-z,  A-Z or a number 0-9
+	var expression = /[^a-z0-9]/g;// expression which finds anything that's NOT a-z or 0-9
 	var alphanumStr;
 	var arrayedStr;
 	var reversedStr;
 
-	
-	alphanumStr = str.replace(expression,"");// deletes any non-alphanumerical characters, by replacing them with ""
-	arrayedString = alphanumStr.split("");// turns the alphanumerical string into an array
-	reversedStr = arrayedString.reverse();// reverses the order of the array
-	reversedStr = reversedStr.join("");//joins back the reversed array into a string
+	alphanumStr = str.toLowerCase().replace(expression,"");// deletes any non-alphanumerical characters, by replacing them with ""
+	arrayedStr = alphanumStr.split("");// turns the alphanumerical string into an array
+	arrayedStr = arrayedStr.reverse();// reverses the order of the array
+	reversedStr = arrayedStr.join("");//joins back the reversed array into a string
 
-	if (alphanumStr === reversedStr){//if the initialal alphanumerical string is equal to the reversed alphanumerical one, return true (is a palindrome);
+	if (alphanumStr === reversedStr){//if the initial alphanumerical string is equal to the reversed alphanumerical one, return true (is a palidrome);
 		return true;
 	}
 	return false;
