e0af006aad940c5b7da842397dbfc03fbe62b949
Silviu Gherman <sil.gherman@gmail.com>
2017-12-08 19:51:52 +0200

    First draft working solution

---
+++ 05-advanced-algorithm-scripting/05-inventory-update/solution.js
@@ -0,0 +1,30 @@
+function updateInventory(inv, deliv) {
+    for(var i = 0; i < deliv.length; i++){ // loops through delivery array
+    	for(var j = 0; j < inv.length; j++){ // loops through inventory array
+    		if(deliv[i][1] === inv[j][1]){ // checks if the delivery name matches inventory item name
+    			inv[j][0] += deliv[i][0]; // update value if so
+    			break; // exits from the loop to not get into the next condition
+    		}
+    		else if(j === (inv.length - 1)){ // if last item has been reaches and no break encountered
+    			inv.push(deliv[i]);
+    			break; // push changes the inv length, so need to either break out or add one to to "j" so the loop never runs in the first palce
+    		}
+    	}
+    	if(!inv.length){ // adds the first item if inventory is empty
+    		inv.push(deliv[i]);
+    	}
+    }
+
+    // Sorting algorithm (first letter only)
+    for(var k = 0; k < inv.length - 1; k++){
+    	var currItemChar = inv[k][1].toLowerCase().charCodeAt(0);
+    	var nextItemChar = inv[k + 1][1].toLowerCase().charCodeAt(0);
+    	var changeValContainer = inv[k];
+    	if (currItemChar > nextItemChar){
+    		inv[k] = inv[k + 1];
+    		inv[k + 1] = changeValContainer;
+    		k = -1;
+    	}
+    }
+    return inv;
+}
